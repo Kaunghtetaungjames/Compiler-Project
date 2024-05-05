6a180b48888e6739fbd3c1252cc6e39ec0d8890a
May Thukha Naing <163281369+Annnnay@users.noreply.github.com>
2024-05-05 12:55:48 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from lexer import Lexer
 from astparser import Parser
 from interpreter import Interpreter
+from typechecker import TypeChecker
 
-# Testing the interpreter
 def main():
-    variables = {}
     while True:
         try:
             text = input('>> ')
@@ -12,11 +11,21 @@ def main():
             break
         if not text:
             continue
+
         lexer = Lexer(text)
         parser = Parser(lexer)
-        interpreter = Interpreter(parser)
-        result = interpreter.interpret()
-        print(result)
+        type_checker = TypeChecker()
+        interpreter = Interpreter(parser, type_checker)
+
+        try:
+            result = interpreter.interpret()
+            print("Interpreter result:", result)
+            print ("Type checking passed successfully.")
+        except Exception as e:
+            print("Error:", e)
 
 if __name__ == '__main__':
     main()
+
+
+
