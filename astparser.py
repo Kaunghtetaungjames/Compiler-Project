5871c02db6a9e879e3f086e5ec3ce237089d8582
yu-kay-khaing-oo <yukaykhaingoo@my.jcu.edu.au>
2024-05-05 15:02:01 +0630

    Add parser and interpreter

---
+++ astparser.py
@@ -0,0 +1,153 @@
+from lexer import TokenTypes
+
+class AST:
+    pass
+
+class Num(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+class Boolean(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+class BinOp(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
+class UnaryOp(AST):
+    def __init__(self, op, expr):
+        self.token = self.op = op
+        self.expr = expr
+
+class Assign(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
+class IfElse(AST):
+    def __init__(self, condition, if_block, else_block=None):
+        self.condition = condition
+        self.if_block = if_block
+        self.else_block = else_block
+
+class WhileLoop(AST):
+    def __init__(self, condition, block):
+        self.condition = condition
+        self.block = block
+
+class Print(AST):
+    def __init__(self, expr):
+        self.expr = expr
+
+class Parser:
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
+
+    def error(self):
+        raise Exception('Invalid Syntax')
+
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
+
+    def factor(self):
+        token = self.current_token
+        if token.type == TokenTypes.INTEGER or token.type == TokenTypes.FLOAT:
+            self.eat(token.type)
+            return Num(token)
+        elif token.type == TokenTypes.BOOLEAN:
+            self.eat(token.type)
+            return Boolean(token)
+        elif token.type == TokenTypes.LPAREN:
+            self.eat(TokenTypes.LPAREN)
+            node = self.expr()
+            self.eat(TokenTypes.RPAREN)
+            return node
+        elif token.type == TokenTypes.MINUS:
+            self.eat(TokenTypes.MINUS)
+            return UnaryOp(token, self.factor())
+        else:
+            return self.variable()
+
+    def term(self):
+        node = self.factor()
+        while self.current_token.type in (TokenTypes.MULTIPLY, TokenTypes.DIVIDE):
+            token = self.current_token
+            self.eat(token.type)
+            node = BinOp(left=node, op=token, right=self.factor())
+        return node
+
+    def expr(self):
+        node = self.term()
+        while self.current_token.type in (TokenTypes.PLUS, TokenTypes.MINUS):
+            token = self.current_token
+            self.eat(token.type)
+            node = BinOp(left=node, op=token, right=self.term())
+        return node
+
+    def equality_expr(self):
+        node = self.expr()
+        if self.current_token.type in (TokenTypes.EQUAL, TokenTypes.NOTEQUAL):
+            token = self.current_token
+            self.eat(token.type)
+            return BinOp(left=node, op=token, right=self.expr())
+        else:
+            return node
+
+    def statement(self):
+        if self.current_token.type == TokenTypes.ID:
+            node = self.assignment_statement()
+        elif self.current_token.type == TokenTypes.IF:
+            node = self.if_statement()
+        elif self.current_token.type == TokenTypes.WHILE:
+            node = self.while_statement()
+        elif self.current_token.type == TokenTypes.PRINT:
+            node = self.print_statement()
+        else:
+            node = self.expr()
+        return node
+
+    def assignment_statement(self):
+        left = self.variable()
+        token = self.current_token
+        self.eat(TokenTypes.ASSIGN)
+        right = self.expr()
+        return Assign(left, token, right)
+
+    def variable(self):
+        token = self.current_token
+        self.eat(TokenTypes.ID)
+        return token
+
+    def if_statement(self):
+        self.eat(TokenTypes.IF)
+        condition = self.expr()
+        if_block = self.statement()
+        if self.current_token.type == TokenTypes.ELSE:
+            self.eat(TokenTypes.ELSE)
+            else_block = self.statement()
+            return IfElse(condition, if_block, else_block)
+        return IfElse(condition, if_block)
+
+    def while_statement(self):
+        self.eat(TokenTypes.WHILE)
+        condition = self.expr()
+        block = self.statement()
+        return WhileLoop(condition, block)
+
+    def print_statement(self):
+        self.eat(TokenTypes.PRINT)
+        expr = self.expr()
+        return Print(expr)
+
+    def parse(self):
+        return self.statement()
---
+++ interpreter.py
@@ -0,0 +1,61 @@
+from lexer import TokenTypes
+
+class Interpreter:
+    def __init__(self, parser):
+        self.parser = parser
+        self.variables = {}
+
+    def visit(self, node):
+        method_name = f'visit_{type(node).__name__}'
+        print(type(node).__name__)
+        visitor = getattr(self, method_name, self.generic_visit)
+        return visitor(node)
+
+    def generic_visit(self, node):
+        raise Exception(f'No visit_{type(node).__name__} method')
+
+    def interpret(self):
+        tree = self.parser.parse()
+        return self.visit(tree)
+
+    def visit_BinOp(self, node):
+        if node.op.type == TokenTypes.PLUS:
+            return self.visit(node.left) + self.visit(node.right)
+        elif node.op.type == TokenTypes.MINUS:
+            return self.visit(node.left) - self.visit(node.right)
+        elif node.op.type == TokenTypes.MULTIPLY:
+            return self.visit(node.left) * self.visit(node.right)
+        elif node.op.type == TokenTypes.DIVIDE:
+            return self.visit(node.left) / self.visit(node.right)
+        elif node.op.type == TokenTypes.EQUAL:
+            return self.visit(node.left) == self.visit(node.right)
+        elif node.op.type == TokenTypes.NOTEQUAL:
+            return self.visit(node.left) != self.visit(node.right)
+
+    def visit_Num(self, node):
+        return node.value
+
+    def visit_Boolean(self, node):
+        return node.value
+
+    def visit_Assign(self, node):
+        var_name = node.left.value
+        value = self.visit(node.right)
+        self.variables[var_name] = value
+        return value
+
+    def visit_If(self, node):
+        if self.visit(node.condition):
+            return self.visit(node.if_block)
+        elif node.else_block:
+            return self.visit(node.else_block)
+
+    def visit_WhileLoop(self, node):
+        result = None
+        while self.visit(node.condition):
+            result = self.visit(node.block)
+        return result
+
+    def visit_Print(self, node):
+        result = self.visit(node.expr)
+        print(result)
